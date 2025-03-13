58f940ec8c98e6998e706624e348c8107a2d992b
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2025-03-13 20:37:36 -0300

    Created project class "hook 2"

--- Estados/estadoAula01/src/App.js
+++ Estados/estadoAula01/src/App.js
@@ -6,17 +6,29 @@ function App() {
 
   const [ativo, setAtivo] = React.useState(false)
   const [dados, setDados] = React.useState({nome: 'André', idade: 30})
-  const [modal, setModal] = React.useState(false);
+  const [modal, setModal] = React.useState(() =>{
+    const ativo = window.localStorage.getItem('ativo');
+      return ativo;
+  });
 
+  let [items, setItems] = React.useState('Teste');
+
+  console.log('Log')
   function handleClick() {
     setAtivo(!ativo)  
     console.log(ativo)
     setDados({...dados, faculdade: "Engenharia"})
   }
 
+  function handleClickItems(){
+    setItems("Outro")
+  }
+
   return (
   <div>
+    <p>{items}</p>
       <div>{modal ? "Modal aberto" : "Modal fechado"}</div>
+      <button onClick={handleClickItems}>Clique aqui</button>
       <ButtonModal setModal={setModal}/>
       <Modal modal={modal} setModal={setModal}/>
       <p>{dados.nome}</p>
