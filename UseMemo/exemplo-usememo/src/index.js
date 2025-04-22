e56a113043a10026d670bc455d7819135247d103
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2025-04-22 18:40:19 -0300

    Created example UseMemo

---
+++ UseMemo/exemplo-usememo/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './AppUseCallback';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <App />
+  </>
+);
+
